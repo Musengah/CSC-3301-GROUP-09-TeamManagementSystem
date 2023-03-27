0e55bf9b14eff2f7493fceb4a6e14a183d50e862
Fidelis Musamba <65792527+DevFidelis@users.noreply.github.com>
2023-03-27 15:45:36 +0200

    added the member login method

--- Main.java
+++ Main.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(welcome());
+        // initialize arrays to store user data
+        String[] admins = new String[1];
+        String[] members = new String[5];
+
+        login(welcome());
     }
 
+    // method to welcome the user and to know what type of user they are.
     public static int welcome() {
-        // instantiate the scanner object and intialize some useful varibales
+        // instantiate the scanner object and intialize some useful varibales.
         Scanner input = new Scanner(System.in);
         boolean flag = true;
         int userOption = 0;
@@ -26,10 +31,35 @@ public class Main {
         // close objects to avaoid resource leak
         input.close();
 
+        // return the user option
         return userOption;
     }
 
-    public void login() {
-        
+    // method to login the user
+    public static void login(int userOption) {
+        // login algorithm (initialize scanner and some useful variables)
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your username?");;
+        String username = input.nextLine();
+        System.out.println("What is your password? ");
+        String password = input.nextLine();
+
+        switch (userOption) {
+            case 1:
+                
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+
+                break;
+        }
     }
 }

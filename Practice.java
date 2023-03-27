178fc9af27ee656fe6cc7b574ab9c28e15ca3724
DevMusengah <123998676+Musengah@users.noreply.github.com>
2023-03-27 15:42:32 +0200

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,45 +1,51 @@
+import java.util.Objects;
 import java.util.Scanner;
-class Group9{
+class GroupNine
+{
+    public static void main(String[] args)
 
-  public static void main(String[] args) 
-  {
-    l
-  }
-  void login() 
-  {
-    System.out.println("Welcome user");
-    System.out.println();
-    System.out.println();
-    System.out.println("For admin login press 1");
-    System.out.println("For normal member login press 2");
-    System.out.println("For help press 3");
-    System.out.println("For signing up press 4");
-  }
-  public void inputAdmin()
-  {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("How many members do you want to input in the team ");
-    int a = scan.nextInt();
-    int s[] = new int[a];
-  }
-  public void inputMember()
-  {
-    Scanner scan = new Scanner(System.in);
-    String password = "Wsegg35%j8";
-    System.out.println("What is your username ");
-    String user = scan.nextLine();
-    System.out.println("What is your password ");
-    String s = scan.nextLine();
-    int i = 3;
-    while (i>0)
     {
-      if(s==password)
-      {
-        System.out.println("Welcome team member "+user);
-        i=-i;
-      }
-      
-
+        login();
+        member();
     }
-  }
+    public static void login()
+    {
+        System.out.println("Welcome user");
+        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        String password = "1234";
+        System.out.println("What is your username ");
+        String user = scan.nextLine();
+        System.out.println("What is your password ");
+        String s = scan.next();
+        int i = 3;
+        while (i>0)
+        {
+            if (Objects.equals(s, password))
+            {
+                System.out.println("Welcome team member "+s);
+                i =-i;
+            }
+            else
+            {
+                System.out.println("Try again");
+                i =-1;
+            }
+        }
+        System.out.println();
+        System.out.println("For admin login press 1");
+        System.out.println("For normal member login press 2");
+        System.out.println("For help press 3");
+        System.out.println("For signing up press 4");
+    }
+    public static void input()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many members do you want to input in the team ");
+        int a = scan.nextInt();
+        int s[] = new int[a];
+    }
+    public static void member()
+    {
+    }
 }
